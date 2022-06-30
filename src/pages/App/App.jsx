9ca1045ec5db44c54c9e68667470f36fe4795557
Lana Kim <lanakim@Lanas-MacBook-Pro.local>
2022-06-30 15:41:26 -0400

    finalized

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+// Components
+import Nav from '../../components/Nav/Nav'
+import Footer from '../../components/Footer/Footer'
+import MovieDetails from '../../components/MovieDetails/MovieDetails'
+import UpdateMovieForm from '../../components/UpdateMovieForm/UpdateMovieForm'
+// Pages
+import Home from '../Home/Home'
+import Login from '../Login/Login'
+import SignUp from '../SignUp/SignUp'
+import Movies from '../Movies/Movies'
+import CreateMovie from '../CreateMovie/CreateMovie'
+// Services
+import * as usersService from '../../utilities/users-service'
+// CSS
+import './App.css';
+
+const App = () => {
+  const [user, setUser] = useState('')
+
+  useEffect(() => {
+    if (usersService.getToken()) setUser(usersService.getUser())
+  }, [])
+
+  return (
+    <div className="App">
+      <Nav user={user} setUser={setUser} logOut={usersService.logOut} />
+
+      {/* client-side route that renders the component instance if the path matches the url in the address bar */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login setUser={setUser} />} />
+        <Route path='/signup' element={<SignUp setUser={setUser} />} />
+        {
+          user &&
+          <>
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/movies/create' element={<CreateMovie />} />
+            <Route path='/movies/:id' element={<MovieDetails />} />
+            <Route path='/movies/:id/edit' element={<UpdateMovieForm />} />
+          </>
+        }
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            This is the Home Page
+        </div>
+    );
+}
+
+export default Home;
